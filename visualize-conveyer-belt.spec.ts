a122df8d39650261f22e85a15f07c4a253764cdd
mathieu cans <mathieu.cans@orange.com>
2026-01-20 15:57:18 +0100

    Size belt done

--- visualize-conveyer-belt.spec.ts
+++ visualize-conveyer-belt.spec.ts
@@ -40,19 +40,49 @@ class Item {
 
 type ConveyerEvent = ConveyorInitializedEvent | ItemAddedEvent
 
-function eventsToVisualization (events: ConveyerEvent[]) {
-    return "_ _ _"
-}
-function visualizationToEvents (outputs: string) {
-    return [ConveyorInitialized(new Belt(3, []))]
+class InitializedVisualizationAcc implements VisualizationAccumulator{
+    constructor (private belt: Belt) {
+
+    }
+
+    consume(event: ConveyerEvent): VisualizationAccumulator {
+        return this;
+    }
+
+    toString() : string {
+        return Array.from( { length: this.belt.size }, () => "_" ).join(" ");
+    }
 }
 
+interface VisualizationAccumulator {
+    consume (event: ConveyerEvent) : VisualizationAccumulator
+}
+
+class EmptyVisualizationAcc implements VisualizationAccumulator {
+    consume (event: ConveyerEvent) {
+        if (event instanceof ConveyorInitializedEvent) {
+            return new InitializedVisualizationAcc(event.belt);
+        }
+        throw 'bad event type'
+    }
+}
+
+function eventsToVisualization (events: ConveyerEvent[]) {
+    const acc = events.reduce((acc, event) => acc.consume(event),
+        new EmptyVisualizationAcc() as VisualizationAccumulator);
+    return acc.toString()
+}
+function visualizationToEvents (outputs: string) {
+    const tokens = outputs.split(" ")
+    return [ConveyorInitialized(new Belt(tokens.length, []))]
+}
 
 describe('visualize-conveyer-belt', () => {
     describe.each([
         [[ConveyorInitialized(new Belt(3, []))], "_ _ _"],
-        [[ConveyorInitialized(new Belt(3, [])), ItemAdded(new Item("a"))], "I(a) _ _"],
-])
+        [[ConveyorInitialized(new Belt(4, []))], "_ _ _ _"],
+        // [[ConveyorInitialized(new Belt(3, [])), ItemAdded(new Item("a"))], "I(a) _ _"],
+    ])
     ('Initial examples %s <-> %s', (events, outputs) => {
         test('eventsToVisualization', () => {
 
